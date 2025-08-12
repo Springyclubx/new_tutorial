b51b318fc02cc7d400732f21f26916efa5285ae4
Guilherme Fernandes <guilherme.fernandes.f04@gmail.com>
2025-08-11 23:57:44 -0300

    Começando a copiar a biblioteca e trocando os nomes para fazer sentido com oque preciso

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:new_tutorial/provider/tutorial_main_state.dart';
+import 'package:provider/provider.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => TutorialMainState(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -26,18 +32,52 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final state = Provider.of<TutorialMainState>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 8,
+            children: <Widget>[
+              _ContainerDefault(color: Colors.red, key: state.redKey),
+              _ContainerDefault(color: Colors.blue, key: state.blueKey),
+              InkWell(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                key: state.greenKey,
+                onTap: () {
+                  state.tutorialCoachMark.show(context: context);
+                },
+                child: _ContainerDefault(
+                  color: Colors.green.withValues(alpha: 0.6),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class _ContainerDefault extends StatelessWidget {
+  const _ContainerDefault({required this.color, super.key});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(8)),
       ),
     );
   }
---
+++ lib/provider/tutorial_main_state.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:new_tutorial/tutorial_page/tutorial_coach_mark_default.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+
+class TutorialMainState extends ChangeNotifier {
+  TutorialMainState() {
+    _init();
+  }
+
+  late final TutorialCoachMarkDefault _tutorialCoachMark;
+
+  final _redKey = GlobalKey();
+  final _greenKey = GlobalKey();
+  final _blueKey = GlobalKey();
+
+  GlobalKey get redKey => _redKey;
+
+  GlobalKey get greenKey => _greenKey;
+
+  GlobalKey get blueKey => _blueKey;
+
+  TutorialCoachMarkDefault get tutorialCoachMark => _tutorialCoachMark;
+
+  Future<void> _init() async {
+    createTutorial();
+  }
+
+  void createTutorial() {
+    _tutorialCoachMark = TutorialCoachMarkDefault(
+      targets: _createTargets(),
+      colorShadow: Colors.red,
+      textSkip: "SKIP",
+      paddingFocus: 10,
+      opacityShadow: 0.5,
+      onFinish: () {
+        print("finish");
+      },
+      onClickTarget: (target) {
+        print('onClickTarget: $target');
+      },
+      onClickTargetWithTapPosition: (target, tapDetails) {
+        print("target: $target");
+        print(
+          "clicked at position local: ${tapDetails.localPosition} - global: ${tapDetails.globalPosition}",
+        );
+      },
+      onClickOverlay: (target) {
+        print('onClickOverlay: $target');
+      },
+      onSkip: () {
+        print("skip");
+        return true;
+      },
+    );
+  }
+
+  List<TargetFocus> _createTargets() {
+    List<TargetFocus> targets = [];
+    targets.add(
+      TargetFocus(
+        keyTarget: _redKey,
+        alignSkip: Alignment.topRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            builder: (context, controller) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Titulo lorem ipsum",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+
+    return targets;
+  }
+}
